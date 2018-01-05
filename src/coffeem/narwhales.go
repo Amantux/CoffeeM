23d0a370640e567db40d749082cd71b98f55dbc7
Amantux <amantux@gmail.com>
2018-01-05 07:02:41 +0000

    Updated code, needed to apply changes, panicked a bit!

--- src/coffeem/narwhales.go
+++ src/coffeem/narwhales.go
@@ -5,10 +5,18 @@ import (
 	"strings"
 	"io/ioutil"
 	"fmt"
-	"github.com/huin/goserial"
+	"go.bug.st/serial.v1"	
 	)
-
+//Find The Arduino
 func findarduino() string{
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		fmt.Println("Error0000:No Ports Found")
+	return
+	}
 	contents, _ := ioutil.ReadDir("/dev")
 	for _, f := range contents {
 		if strings.Contains(f.Name(), "ttyUSB")|| strings.Contains(f.Name(), "tty.usbserial"){
@@ -17,6 +25,16 @@ func findarduino() string{
 	}
 	return "Error0001:Arduino Connection not found"
 }
+
+//Open The Serial Channels
+//Define Serial Comms
+mode := &serial.Mode{
+    BaudRate: 9600,
+    Parity:   serial.NoParity,
+    DataBits: 8,
+    StopBits: serial.OneStopBit,
+}
+
 func sendArduino(toUUID string, []command string, fromUUID string, serialPort io.ReadWriteCloser) error{
 	if serialPort == nil {
 		return "Error0002:No Serial Found"
