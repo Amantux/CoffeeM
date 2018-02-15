3f4f7a861e2c973f2233feb94da36bf3625bc3f9
Amantux <amantux@gmail.com>
2018-02-15 10:48:42 -0500

    Added in Relay function for testing Allowedconnections from any IP address Solved minor erros with Channelmanagement

--- coffeem/coffeem.go
+++ coffeem/coffeem.go
@@ -2,58 +2,83 @@ package main
 
 import (
 	"CoffeeM/coffeem/tcp"
-	"CoffeeM/coffeem/udp"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 )
 
-func msgPrint(msg <-chan udp.Msg, wg *sync.WaitGroup) {
-	for m := range msg {
-		fmt.Println(m.Pld)
-	}
-	wg.Done()
-}
-func CheckError(err error) {
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(0)
-	}
-}
-func AddChannel(TargetChan chan string, str string) {
-	fmt.Println("Pree add")
-	fmt.Println(str)
-	TargetChan <- str
-	fmt.Println("Added")
-}
-
 func main() {
 	cfg := tcp.Config{
-		IPListen: "127.0.0.1",
+		IPListen: "0.0.0.0",
 		Port:     10001,
 	}
-	msg := make(chan tcp.Msg)
-	defer close(msg)
 	lg := log.New(os.Stderr, "Pi:", log.LstdFlags)
 	term := make(chan bool)
 	go termRasPI(term)
 	status := make(chan string)
 	defer close(status)
 	var wg sync.WaitGroup
-	udp.Start(cfg, msg, lg, &wg, status, term)
-	<-term
+	msgOut, err := tcp.Start(cfg, lg, &wg, status, term)
+	if err != nil {
+		lg.Fatalf("Fatal: Could not start server: '%s'.", err.Error())
+		os.Exit(1)
+	}
+	go relay(msgOut, lg)
+	//go msgPrint(msgOut, lg, &wg)
+	// coordinate graceful terination
+	select {
+	case <-term:
+	case <-status:
+		// suicide call
+		termMyself()
+		<-term
+	}
 	wg.Wait()
+}
+func relay(msg <-chan tcp.Msg, lg *log.Logger) {
+	var arduino *tcp.Msg
+	var Nexus *tcp.Msg
+	for m := range msg {
+		payload := string(m.Pld)
+		lg.Printf("Info: Received Message:'%s'.", payload)
+		if strings.HasPrefix(payload, "Arduino") {
+			arduino = &m
+		} else if strings.HasPrefix(payload, "Nexus") && Nexus != nil {
+			Nexus = &m
+		}
+		if Nexus != nil && arduino != nil {
+			err := arduino.Reply(Nexus.Pld)
+			if err != nil {
+				lg.Printf("Error: line 49: '%s'.", err.Error())
+			}
+		}
+	}
+}
+
+func msgPrint(msg <-chan tcp.Msg, lg *log.Logger, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for m := range msg {
+		lg.Printf("Info: Received message: '%s'.", string(m.Pld))
+	}
+}
+
+func termMyself() {
+	pid := os.Getpid()
+	mySelf, _ := os.FindProcess(pid)
+	mySelf.Signal(os.Interrupt)
 }
 
 func termRasPI(term chan bool) {
+
 	osSig := make(chan os.Signal)
 	defer close(osSig)
-	defer close(t)
+	defer close(term)
 	// Currently listens for only SIGTERM as it's available on every
 	// flavor of Linux.
 	signal.Notify(osSig, os.Interrupt)
 	// blocks until it receives at least one signal
 	<-osSig
+	signal.Stop(osSig)
 }
