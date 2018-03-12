078ad537efec728449d72e8ec57d089d38a3f786
Amantux <amantux@gmail.com>
2018-03-11 20:08:25 -0400

    fix build automation to create static executable for specifig OS and architecture

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-Config.sh
-docker run --rm -v "../coffeem":/usr/src/myapp -w /usr/src/myapp -e GOOS=linux -e GOARCH=armv64 golang:1.9.2 go build -v
+./Config.sh
+coffeePath="$( dirname "$( pwd )")"
+docker run --rm -v "$coffeePath":/go/src/CoffeeM -w /go/src/CoffeeM/coffeem -e GOOS=linux -e GOARCH=amd64 golang:1.9.2 go build -v -a -tags netgo -ldflags '-w -extldflags "-static"' -o coffeem 
+docker build -t coffeem  ../coffeem
+
