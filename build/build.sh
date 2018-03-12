4082a937a5f2b78e2cc8472a37edbf66c4e3a922
Amantux <amantux@gmail.com>
2018-03-12 12:06:39 -0400

    Config must bedone with within compiler container, simplify messages, fail fast, accept go architecture setting as first parameter default to raspPi3, remove -e environment variable settings on docker run as dependency update (dep endure) must be run within architecture supported by container

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-./Config.sh
+# if any error detected, stop build
+set -e
+# assume rasberryPi arm V8 processor
+GOARCH_ARG="${1:-arm64}"
+# get absolute path to project directory assuming running build.sh from build directory
 coffeePath="$( dirname "$( pwd )")"
-docker run --rm -v "$coffeePath":/go/src/CoffeeM -w /go/src/CoffeeM/coffeem -e GOOS=linux -e GOARCH=amd64 golang:1.9.2 go build -v -a -tags netgo -ldflags '-w -extldflags "-static"' -o coffeem 
-docker build -t coffeem  ../coffeem
+# run compile
+docker run --rm -v "$coffeePath":/go/src/CoffeeM -w /go/src/CoffeeM/coffeem  golang:1.9.2 /go/src/CoffeeM/build/build_go.sh "${GOARCH_ARG}" 
+# put static binary coffeem in its own container
+echo 'INFO: Creating docker container'
+docker build -t coffeem  ../coffeem >/dev/null
+echo "INFO: Created docker container: 'coffeem:latest'"
 
